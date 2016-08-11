ec1ca87bff7c449ddd5bcd13cd1a6a570ab809a9
Evan Tahler <evantahler@gmail.com>
2016-08-11 12:24:19 -0700

    the file

---
+++ initializers/logger.js
@@ -0,0 +1,32 @@
+module.exports = {
+  loadPriority:  1000,
+
+  initialize: function(api, next){
+    var transport = {
+      name: 'logger',
+      description: 'for testing messages by sending them to a log file',
+
+      requiredDataKeys: {
+        person: ['firstName', 'lasName']
+      },
+
+      campaignVariables: [
+        'logPrefix',
+        'logLevel',
+      ],
+
+      deliver: function(payload, person, callback){
+        var message = '';
+        message += '[' + payload.logPrefix + ' | to ' + person.data.data.firstName + ' ' + person.data.data.firstName + '] ';
+        message += payload.body;
+
+        api.log(message, payload.logLevel);
+        return callback();
+      }
+    }
+
+    api.transports.push(transport);
+
+    next();
+  },
+};
